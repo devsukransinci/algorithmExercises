568d169ac1e3b63db84b14fad6c8778a28e9aa52
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-03-17 17:39:46 +0300

    Update Main.java

--- algorith_Exercises/BudgetAllocation/src/main/java/org/example/Main.java
+++ algorith_Exercises/BudgetAllocation/src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("How much you spend?");
+        Scanner scan =new Scanner(System.in);
+        double total = scan.nextDouble();
+        double sum =0;
+        int i=0;
+        ArrayList<Double> proportion =new ArrayList<Double>();
+        System.out.println("Enter your proportion of various expeses");
+        System.out.println("The system stops if cumulative proportion exceeds 100%");
+        do {
+            System.out.println("Your proportion of expense" + (i+1) + ":");
+            double value =scan.nextDouble();
+            proportion.add(value);
+            sum += proportion.get(i);
+            i++;
+
+        } while (sum <=100);
+        scan.close();
+        if (sum > 100){
+            double cumulativeSum =0.0;
+            for (int j = 0; j < proportion.size() - 1; j++){
+                cumulativeSum += proportion.get(j);
+            }
+            proportion.set(proportion.size()-1 ,
+                    100.0 - cumulativeSum );
+        }
+        for (double value :proportion ){
+            double expense =value*total/100.0;
+            System.out.println("Your"+ value + "% equals $" + expense);
+        }
     }
 }
