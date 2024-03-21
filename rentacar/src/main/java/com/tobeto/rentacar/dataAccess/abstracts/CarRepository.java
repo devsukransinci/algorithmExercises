42523f005c72d6ec63c91217955a1ff511ae9d69
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-03-21 23:35:00 +0300

    Delete rentacar/src/main/java/com/tobeto/rentacar/dataAccess/abstracts/CarRepository.java

--- rentacar/src/main/java/com/tobeto/rentacar/dataAccess/abstracts/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tobeto.rentacar.dataAccess.abstracts;
-import com.tobeto.rentacar.entities.concretes.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car,Integer>  {
-
-
-
-}
